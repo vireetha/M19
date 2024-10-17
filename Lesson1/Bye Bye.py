79bf1565336e0945a64e67e2e353863345d160fc
vireetha <dudalaviree69@gmail.com>
2024-10-17 12:30:07 +0530

    20

---
+++ Lesson1/Bye Bye.py
@@ -0,0 +1,12 @@
+valid = False
+
+while not valid:
+  try:
+    n = int(input("Enter a number: "))
+    
+    while n%2 == 0:
+      print("bye")
+      valid = True
+
+  except ValueError:
+    print("Invalid")
